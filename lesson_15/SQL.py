c47f842ea001506414c442a17e1f16cfae042b4e
Master997-oss <123649891+Master997-oss@users.noreply.github.com>
2023-07-20 20:12:22 +0400

    Add files via upload

---
+++ lesson_15/SQL.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+
+with sqlite3.connect('db.sqlite3') as con:
+    cur = con.cursor()
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS sportsman (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_year INTEGER NOT NULL
+    );
+    ''')
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS competition (
+        id INTEGER PRIMARY KEY,
+        name TEXT(30) UNIQUE,
+        date INTEGER NOT NULL,
+        location TEXT 
+    );
+    ''')
+
+    cur.execute('''
+    CREATE TABLE IF NOT EXISTS result (
+        id INTEGER PRIMARY KEY,
+        competition_id INTEGER NOT NULL,
+        sportsman_id  INTEGER NOT NULL,
+        world_record  INTEGER NOT NULL,
+        personal_record   INTEGER NOT NULL,
+        FOREIGN KEY(competition_id) REFERENCES competition(id),
+        FOREIGN KEY(sportsman_id) REFERENCES sportsman(id)
+    );
+    ''')
+
+    con.commit()
